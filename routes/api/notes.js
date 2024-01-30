efaaf6eed9b917f685d2ea870661c4ea6827f6e9
Jordan <jordane.burton@gmail.com>
2024-01-30 09:43:45 -0600

    added functionality to get notes data from db

--- routes/api/notes.js
+++ routes/api/notes.js
@@ -1,15 +1,12 @@
 const notes = require('express').Router();
-const fsUtils = require('../../helpers/fsUtils');
-const readFromFile = util.promisify(fs.readFile);
+const { readFromFile, readAndAppend } = require('../../helpers/fsUtils');
 
 // GET Route for notes data
 notes.get('/', (req, res) => {
     // Log that a GET request was received
     console.info(`${req.method} request received for notes data`);
-    // INVALID LINE, USE FS METHOD
 
-    readFromFile('./db/feedback.json').then((data) => res.json(JSON.parse(data)));
-    // return res;
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
 
@@ -21,6 +18,31 @@ notes.post('/', (req, res) => {
     // Log that a POST request was received
     console.info(`${req.method} request received to submit notes`);  
     
+    const { noteTitle, noteText } = req.body;
+
+  // If all the required properties are present
+    if (noteTitle && noteText) {
+        // Variable for the object we will save
+        const newNote = {
+            title,
+            text,
+        // email,
+        // feedbackType,
+        // feedback,
+        // feedback_id: uuid(),
+        };
+
+        // readAndAppend(newFeedback, './db/feedback.json');
+
+        const response = {
+            status: 'success',
+            body: newNote,
+        };
+
+        res.json(response);
+    } else {
+        res.json('Error in posting new note');
+    }
 });
 
 module.exports = notes;
