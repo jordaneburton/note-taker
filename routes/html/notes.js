b6f45d0c58e41fa639fdd8b07222100b0ae0996d
Jordan <jordane.burton@gmail.com>
2024-01-29 23:25:21 -0600

    fixed path routing in html/notes

--- routes/html/notes.js
+++ routes/html/notes.js
@@ -5,7 +5,7 @@ const notes = express();
 
 // GET Route for notes page
 notes.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
+  res.sendFile(path.join(__dirname, '../../../public/notes.html'))
 );
 
 module.exports = notes;
